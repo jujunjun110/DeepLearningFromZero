05701382103240b8ec0a3be60d1923cf3979d676
Jun Ito <jun_ito@voyagegroup.com>
2020-04-21 08:20:39 +0900

    Add: softmax

--- main.py
+++ main.py
@@ -103,5 +103,12 @@ def relu(x):
     return np.maximum(x, 0)
 
 
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    return exp_a / sum_exp_a
+
+
 if __name__ == "__main__":
     main()
