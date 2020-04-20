4d655f7a7fa03c89783389abebb4bc267d64d59a
Jun Ito <jun_ito@voyagegroup.com>
2020-04-21 00:42:58 +0900

    Update: 3層ニューラルネットワークを実装した

--- main.py
+++ main.py
@@ -3,7 +3,44 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    pass
+    network = init_network()
+    x = np.array([1.0, 0.5])
+    y = forward(network, x)
+    print(y)
+
+
+def init_network():
+    network = {}
+
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['b2'] = np.array([0.1, 0.2])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+
+    a3 = np.dot(z2, W3) + b3
+
+    y = identity_function(a3)
+    return y
+
+
+def identity_function(x):
+    return x
 
 
 def AND(x1, x2):
@@ -52,6 +89,18 @@ def XOR(x1, x2):
     s1 = NAND(x1, x2)
     s2 = OR(x1, x2)
     return AND(s1, s2)
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(x, 0)
 
 
 if __name__ == "__main__":
