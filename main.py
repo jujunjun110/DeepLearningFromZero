620c00e08806926c3432513a768181ad11a185f6
Jun Ito <jun_ito@voyagegroup.com>
2020-04-20 23:55:46 +0900

    Update: ANDの実装をかっこいい感じに変更

--- main.py
+++ main.py
@@ -2,27 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plotsample():
-    x = np.arange(0, 6, 0.1)
-    y1 = np.sin(x)
-    y2 = np.cos(x)
-
-    plt.plot(x, y1, label="sin")
-    plt.plot(x, y2, label="cos")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("sin&cos")
-    plt.legend()
-    plt.show()
-
-
 def main():
     pass
 
 
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    if w1 * x1 + w2 * x2 > theta:
+    w1, w2 = 0.5, 0.5
+    b = -0.7
+
+    x = np.array([x1, x2])
+    w = np.array([w1, w2])
+    tmp = np.sum(w * x) + b
+
+    if tmp > 0:
         return 1
 
     return 0
