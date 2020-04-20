844bb5cc1963afb7d170445179d7bc65482b6a1a
Jun Ito <jun_ito@voyagegroup.com>
2020-04-21 08:26:10 +0900

    Add: softmax_test

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 import unittest
 import main
+import numpy as np
 
 
 class TestMain(unittest.TestCase):
@@ -58,6 +59,14 @@ class TestMain(unittest.TestCase):
                 x1, x2, expected = case
                 actual = main.XOR(x1, x2)
                 self.assertEqual(actual, expected)
+
+    def test_softmax(self):
+        case = np.array([0.3, 2.9, 4.0])
+        actual = main.softmax(case)
+        expected = [0.01821127, 0.24519181, 0.73659691]
+
+        self.assertTrue(np.allclose(actual, expected))
+        self.assertEqual(actual.sum(), 1.0)
 
 
 if __name__ == "__main__":
