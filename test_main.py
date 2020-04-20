f89c16987175ba83e96b89fae7a166d2a2b6e89f
Jun Ito <jun_ito@voyagegroup.com>
2020-04-20 23:57:43 +0900

    Add: NAND/OR

--- test_main.py
+++ test_main.py
@@ -17,6 +17,20 @@ class TestMain(unittest.TestCase):
                 actual = main.AND(x1, x2)
                 self.assertEqual(actual, expected)
 
+    def test_OR(self):
+        cases = [
+            [0, 0, 0],
+            [1, 0, 1],
+            [0, 1, 1],
+            [1, 1, 1],
+        ]
+
+        for case in cases:
+            with self.subTest(case=case):
+                x1, x2, expected = case
+                actual = main.OR(x1, x2)
+                self.assertEqual(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
