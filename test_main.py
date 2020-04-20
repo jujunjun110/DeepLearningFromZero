b7800631cdd3b30b53a2adc3d1884287af1e7766
Jun Ito <jun_ito@voyagegroup.com>
2020-04-20 23:52:11 +0900

    Add: ANDのテストケースを作成

---
+++ test_main.py
@@ -0,0 +1,22 @@
+import unittest
+import main
+
+
+class TestMain(unittest.TestCase):
+    def test_AND(self):
+        cases = [
+            [0, 0, 0],
+            [1, 0, 0],
+            [0, 1, 0],
+            [1, 1, 1],
+        ]
+
+        for case in cases:
+            with self.subTest(case=case):
+                x1, x2, expected = case
+                actual = main.AND(x1, x2)
+                self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
