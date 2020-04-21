705e941aeceec94eb330a81b52290477ebfc03a9
Jun Ito <jun_ito@voyagegroup.com>
2020-04-21 09:52:16 +0900

    Add: batch execution

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ from PIL import Image
 
 
 def main():
+    exec_parallel()
+
+
+def exec_parallel():
+    x, t = get_data()
+    network = init_network()
+    batch_size = 100
+    accuracy_count = 0
+
+    for i in range(0, len(x), batch_size):
+        x_batch = x[i: i + batch_size]
+        y_batch = predict(network, x_batch)
+        p = np.argmax(y_batch, axis=1)
+        accuracy_count += np.sum(p == t[i:i+batch_size])
+
+    print(f"Accuracy: {accuracy_count / len(x)}")
+
+
+def exec_sequentially():
     x, t = get_data()
     network = init_network()
     accuracy_count = 0
