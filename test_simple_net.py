272074208c75500590a670dbeb8262c61c01df63
Jun Ito <jun_ito@voyagegroup.com>
2020-04-23 00:21:19 +0900

    Add: SimpleNet

--- test_simple_net.py
+++ test_simple_net.py
@@ -1,26 +1,15 @@
 import unittest
-from simple_net import SimpleNet
 import numpy as np
+
+from simple_net import SimpleNet
+from util import numerical_grad
 
 
 class TestSimpleNet(unittest.TestCase):
     def test_assert_true(self):
         net = SimpleNet()
-        print(net.w)
-
-        x = np.array([0.6, 0.9])
-        p = net.predict(x)
-        print(p)
-
-        print(np.argmax(p))
-
-        t = np.array([0, 0, 1])
-
-        loss = net.loss(x, t)
-        print(loss)
-
         self.assertTrue(True)
 
 
 if __name__ == "__main__":
-    pass
+    unittest.main()
