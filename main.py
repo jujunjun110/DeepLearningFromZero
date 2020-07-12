bfe50c46c18272299e17e3e618f509932f37203d
Jun Ito <jun_ito@voyagegroup.com>
2020-07-12 17:35:06 +0900

    Add: Convolution, col2im

--- main.py
+++ main.py
@@ -174,5 +174,57 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     return col
 
 
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """
+    Parameters
+    ----------
+    col :
+    input_shape : 入力データの形状（例：(10, 1, 28, 28)）
+    filter_h :
+    filter_w
+    stride
+    pad
+    Returns
+    -------
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h,
+                      filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W  # 重み
+        self.b = b  # バイアス
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+
+        out_h = int(1 + (H + 2 * self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2 * self.pad - FH) / self.stride)
+
+        col = im2col(x, FH, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        return out
+
+
 if __name__ == "__main__":
     main()
